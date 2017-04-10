0d4495a263805469827dcd14bd863e864816919d
btekse <btekse@gmail.com>
2017-04-10 18:05:27 +0200

    add(app) : Cuboid Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Balázs on 2017. 04. 10..
  */
 public class Main {
+  public static void main(String[] args) {
+    Cuboid cuboid = new Cuboid(2, 3,4);
+
+    System.out.println("surface: " + cuboid.getSurface());
+    System.out.println("volume: " + cuboid.getVolume());
+
+  }
 }
