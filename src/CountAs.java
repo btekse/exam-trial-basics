4e3100e8d488102abf5bfd9043606d54452d109c
btekse <btekse@gmail.com>
2017-04-10 16:29:47 +0200

    add(app) : CountAs string counter

--- src/CountAs.java
+++ src/CountAs.java
@@ -1,3 +1,5 @@
+import com.sun.deploy.util.StringUtils;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,15 +13,27 @@ public class CountAs {
     List<String> fileInput = new ArrayList<>();
 
     try {
-      Path filepath = Paths.get("cuntas/afile.txt");
+      Path filepath = Paths.get("countas/afile.txt");
       fileInput = Files.readAllLines(filepath);
       System.out.println("File reading OK.");
     } catch (IOException e) {
       System.out.println("Something wrong:" + e.getClass());
     }
+    String a = fileInput.toString();
+    int charCount  = a.length();
+    System.out.println(charCount);
 
 
-    System.out.println(fileInput);
+    //System.out.println(fileInput);
+
+    /*Path filepath = Paths.get("countas/afile.txt");
+    String text = filepath.toString();
+    char[] a = text.toCharArray();
+    int sz = a.length;
+
+    System.out.println(sz);*/
+
+
   }
 
 }
