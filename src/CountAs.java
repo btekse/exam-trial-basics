6db4d10d87511df15d0ffa5aa7d703b4058edc9d
btekse <btekse@gmail.com>
2017-04-10 15:39:13 +0200

    add(app) : CountAs reading OK

--- src/CountAs.java
+++ src/CountAs.java
@@ -1,8 +1,25 @@
-public class CountAs {
-  public final static String FILE_NAME = "afile.txt";
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-  private static void findLetter() {
-    
+public class CountAs {
+  public static void main(String[] args) {
+
+    List<String> fileInput = new ArrayList<>();
+
+    try {
+      Path filepath = Paths.get("cuntas/afile.txt");
+      fileInput = Files.readAllLines(filepath);
+      System.out.println("File reading OK.");
+    } catch (IOException e) {
+      System.out.println("Something wrong:" + e.getClass());
+    }
+
+
+    System.out.println(fileInput);
   }
 
 }
